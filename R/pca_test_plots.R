e3d501fcfde56193e8fdba6710d1dae9a9d50163
Christian Supsup <supsupchristian@gmail.com>
2025-03-29 17:46:41 -0500

    Update pca_test_plots.R

--- R/pca_test_plots.R
+++ R/pca_test_plots.R
@@ -140,7 +140,7 @@ plot_variance_explained <- function(pca_test, pc_max = NA, percent = TRUE) {
       y = glue("Variance Explained ({var_formant})"),
       caption = glue("")
     ) +
-    theme(panel.background = element_blank(), axis.line = element_line(colour = "grey"),legend.key = element_blank(), legend.position = c(0.80, 0.9), 
+    theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),legend.key = element_blank(), legend.position = c(0.80, 0.9), 
           axis.text=element_text(size=15), axis.title=element_text(size=15), legend.text = element_text(size=12))
 
 }
@@ -396,7 +396,7 @@ plot_loadings <- function(
       y = glue("PC{pc_no} Index Loading"),
       x = ""
     ) +
-    theme(panel.background = element_blank(), axis.line = element_line(colour = "grey"),legend.key = element_blank(), legend.position = "", 
+    theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),legend.key = element_blank(), legend.position = "", 
           axis.text=element_text(size=15), axis.title=element_text(size=15), legend.text = element_text(size=12))
 
 
