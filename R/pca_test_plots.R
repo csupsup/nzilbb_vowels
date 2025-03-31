6903f8ecc259aa60579e9849b7b49e047afce36a
Christian Supsup <supsupchristian@gmail.com>
2025-03-31 16:34:53 -0500

    Update pca_test_plots.R

--- R/pca_test_plots.R
+++ R/pca_test_plots.R
@@ -142,7 +142,7 @@ plot_variance_explained <- function(pca_test, pc_max = NA, percent = TRUE) {
       caption = glue("")
     ) +
     theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),legend.key = element_blank(), legend.position = c(0.80, 0.9), 
-          axis.text=element_text(size=15), axis.title=element_text(size=15), legend.text = element_text(size=12))
+          axis.text=element_text(size=15), axis.title=element_text(size=15), legend.text = element_text(size=15))
 
 }
 
